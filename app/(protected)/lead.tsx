a868bd4176cf84b1b3ed84b96a3b76496c1d02fb
tarikulislam <tarikulislam4145@gmail.com>
2026-06-11 23:37:10 +0530

    Working

---
+++ app/(protected)/lead.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const lead = () => {
+  return (
+    <View>
+      <Text>lead</Text>
+    </View>
+  );
+};
+
+export default lead;
