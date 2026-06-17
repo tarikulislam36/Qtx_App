57d0e85144497161e8ffa273a6820559b8fce6d5
tarikulislam <tarikulislam4145@gmail.com>
2026-06-17 13:03:46 +0530

    UI

--- app/(protected)/_layout.tsx
+++ app/(protected)/_layout.tsx
@@ -4,10 +4,9 @@ export default function Layout() {
   return (
     <Tabs>
       <Tabs.Screen name="dashboard" options={{ title: "Home" }} />
-
+      <Tabs.Screen name="invoice" options={{ title: "Invoice" }} />
       <Tabs.Screen name="lead" options={{ title: "Leads" }} />
       <Tabs.Screen name="chat" options={{ title: "Chat" }} />
-      <Tabs.Screen name="invoice" options={{ title: "Invoice" }} />
     </Tabs>
   );
 }
